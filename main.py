0072b779a2cc177a03f8a5ebb0f9e51de5b54d40
unknown <leo.chely@live.fr>
2019-11-27 18:05:38 -0700

    Video read

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import numpy as np
 import cv2
- 
-filename = ""
+
+cap = cv2.VideoCapture('Lane Detection Test Video 01.mp4'
+
+while(capi.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+cap.release()
+cv2.destroyAllWindows()
