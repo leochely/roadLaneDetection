aebccd19402a5b441fbfce40a8249963d5ea5a43
Curtis Burke <cburke@mymail.mines.edu>
2019-12-01 15:35:13 -0700

    testing commit and push from pycharm

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import numpy as np
 import cv2
+import functions
 
 # Video input
-cap = cv2.VideoCapture('Lane Detection Test Video 01.mp4'
+cap = cv2.VideoCapture('Lane Detection Test Video 01.mp4')
 
 # Video output
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+output = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
 
 # Processing video
-while(capi.isOpened()):
+while(cap.isOpened()):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
