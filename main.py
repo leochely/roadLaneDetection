ccb73c660f09dc4d7548077e452488b2685f9fb4
unknown <leo.chely@live.fr>
2019-12-06 11:47:41 -0700

    Test with .avi format

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import cv2
 import functions
 
 # Video input
-cap = cv2.VideoCapture('Lane Detection Test Video 01.mp4')
+cap = cv2.VideoCapture('Lane Detection Test Video 01.avi')
 
 # Video output
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+output = cv2.VideoWriter('output.avi', fourcc, 30.0, (1280,720))
 
 # Processing video
 while(cap.isOpened()):
     ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray', gray)
+        # Writing original frame with the highlighted lines to output
+        output.write(gray)
+    else:
+        break
 
 cap.release()
+output.release()
 cv2.destroyAllWindows()
