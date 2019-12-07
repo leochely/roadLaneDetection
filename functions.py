ae2d8c6f6f2626ba85f2dbe04cbcd1e7b2f1544c
Curtis Burke <cburke@mymail.mines.edu>
2019-12-07 12:28:31 -0700

    Found Hough lines in warped and unwarped ROI

--- functions.py
+++ functions.py
@@ -4,36 +4,68 @@ import cv2
 def findLaneLines(frame):
 
     # In the order of top left, top right, bottom right, bottom left
+    roi_pts = np.array([[590, 530], [682, 530], [1100, 660], [124, 660]], np.int32)
+
+    # In the order of top left, top right, bottom right, bottom left
     src_pts = np.float32(
         [
-            [590.0, 530.0],
-            [682.0, 530.0],
-            [1100.0, 660.0],
-            [124.0, 660.0],
+            [520.0, 560.0],
+            [740.0, 560.0],
+            [1000.0, 700.0],
+            [270.0, 700.0],
         ]
     )
 
     # Warp lane in front of car to bird eye view
     warped_img, M, Minv = warp_image(frame, src_pts)
+    img = warped_img
+    ShowImage("warped", img)
+    usingUnwarped = False
 
-    # Threshold warped image
-    warped_img_gray = cv2.cvtColor(warped_img, cv2.COLOR_RGB2GRAY)
-    canny_img = cv2.Canny(warped_img_gray, 100, 200)
+    # Threshold warped image (grayscale, and binary)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+    # Canny Edge Detection
+    img = cv2.Canny(img, 100, 200)
+    ShowImage('canny', img)
+
+    # Apply Gaussian Blur
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+
+    if usingUnwarped == True:
+        # Region of Interest - For un-warped frame
+        # Create mask
+        mask = np.zeros_like(img)
+        # Fill mask
+        cv2.fillPoly(mask, [roi_pts], 255) # This is the problem....
+        # Join mask and img
+        masked_img = cv2.bitwise_and(img, mask)
+        ShowImage('ROI', masked_img)
+        img = masked_img
+
+
 
     # Hough Lines
     minLineLength = 1100
     maxLineGap = 10
-    lines = cv2.HoughLines(canny_img,1,np.pi/180,100)
-    for x1,y1,x2,y2 in lines[0]:
-        cv2.line(warped_img_gray,(x1,y1),(x2,y2),(0,255,0),2)
+    #lines = cv2.HoughLines(masked_img, 1, np.pi/180, 100)
+
+    #                          edges   rho   theta   thresh  min length, max gap:
+    lines = cv2.HoughLinesP(img, 1, np.pi/180, 10,      100,         10)
+
+    # Draw Hough Lines
+    for line in lines:
+        coords = line[0]
+        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
 
     # Display Warped image
-    ShowImage('warped_img', canny_img)
+    ShowImage('Frame with lines', img)
 
     # FIXME: Just for now so we have a return value
-    lane_pts = src_pts
+    lane_pts = roi_pts
 
-    return lane_pts, M
+    return lane_pts
 
 def warp_image(img, src_pts):
     height, width = img.shape[0:2]
