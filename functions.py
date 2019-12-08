db88007e655d47521d60052c928d8a13f479ed21
Curtis Burke <cburke@mymail.mines.edu>
2019-12-08 13:02:41 -0700

    Added imwrites after steps (for report images) - commented after

--- functions.py
+++ functions.py
@@ -4,6 +4,8 @@ import cv2
 
 def findLaneLines(frame):
 
+    #cv2.imwrite('Output_Images/frame.jpg', frame)
+
     # In the order of top left, top right, bottom right, bottom left
     src_pts = np.float32(
         [
@@ -14,31 +16,25 @@ def findLaneLines(frame):
         ]
     )
 
-    usingWarped = True
+    # Warp lane in front of car to bird eye view
+    warped_img, M, Minv = warp_image(frame, src_pts)
+    img = warped_img.copy()
+    #ShowImage("Warped Image", warped_img)
+    #cv2.imwrite('Output_Images/warpped.jpg', img)
 
-    if usingWarped:
-        # Warp lane in front of car to bird eye view
-        warped_img, M, Minv = warp_image(frame, src_pts)
-        img = warped_img
-        #ShowImage("Warped Image", warped_img)
-
-        # ROI in Warped Image (works great for the whole video)
-        roi_pts = np.array([[200, 0], [1000, 0], [1000, 720], [200, 720]], np.int32)
-
-    else:
-        img = frame
-
-        # In the order of top left, top right, bottom right, bottom left
-        roi_pts = np.array([[590, 530], [682, 530], [1100, 660], [124, 660]], np.int32)
+    # ROI in Warped Image
+    roi_pts = np.array([[200, 0], [1000, 0], [1000, 720], [200, 720]], np.int32)
 
 
     # Threshold warped image (grayscale, and binary)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    #cv2.imwrite('Output_Images/binary.jpg', img)
 
     # Canny Edge Detection
     img = cv2.Canny(img, 100, 200)
     #ShowImage('Canny Edge Image', img)
+    #cv2.imwrite('Output_Images/canny.jpg', img)
 
     # Apply Gaussian Blur
     img = cv2.GaussianBlur(img, (5, 5), 0)
@@ -61,6 +57,7 @@ def findLaneLines(frame):
         coords = line[0]
         cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 1)
     #ShowImage("Hough Lines", img)
+    #cv2.imwrite('Output_Images/hough.jpg', img)
 
     # Convert Hough lines for a single Left and Right Line
     left_line, right_line = houghLinestoLaneLines(img, lines)
@@ -69,9 +66,11 @@ def findLaneLines(frame):
     overlayed_img = overlay(warped_img, left_line)
     overlayed_img = overlay(warped_img, right_line)
     #ShowImage("Overlayed on Warped", overlayed_img)
+    #cv2.imwrite('Output_Images/overlayed_warpped.jpg', overlayed_img)
 
     # Unwarp Birds-eye View (M_inverse)
     unWarped_img = cv2.warpPerspective(warped_img, Minv, (frame.shape[1], frame.shape[0]))
+    #cv2.imwrite('Output_Images/unwarpepd.jpg', unWarped_img)
 
     # Create Region of interest for masking
     unWarped_roi = np.array([src_pts[0], src_pts[1], src_pts[2], src_pts[3]], np.int32)
@@ -79,7 +78,7 @@ def findLaneLines(frame):
     # Combine frame and unWarped image with a mask around the ROI
     combinded_img = combineImages(frame, unWarped_img, unWarped_roi)
     #ShowImage("Comb", combinded_img)
-
+    #cv2.imwrite('Output_Images/combined.jpg', combinded_img)
 
     return combinded_img
 
